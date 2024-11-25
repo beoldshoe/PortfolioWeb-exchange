d744648a724ed5341d4c410a822ecf4dbdc0020e
beoldshoe <howeve18@gmail.com>
2024-11-25 13:38:35 +0200

    Intial Commit: Settings

---
+++ portfolioweb-exchange/src/pages/IntroducePage.js
@@ -0,0 +1,7 @@
+const IntroducePage = () => {
+    return (
+        <div>김현수</div>
+    )
+}
+
+export default IntroducePage;
---
+++ portfolioweb-exchange/src/routes.js
@@ -0,0 +1,12 @@
+import IntroducePage from './pages/IntroducePage';
+
+const routes = [
+    {
+        path: '/',
+        component: IntroducePage
+    },
+
+
+]
+
+export default routes;
